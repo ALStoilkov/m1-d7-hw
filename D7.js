9aa967a81d6de59f2aaa2fc4f25c2e63be84ac33
ALStoilkov <antonistoilkov@gmail.com>
2021-02-10 01:48:36 +0000

    v.2 (missing ex 13)

--- D7.js
+++ D7.js
@@ -89,35 +89,52 @@ function paintItGreen(){
 // Make the heading of the page change color every time the user clicks on it.
 // */
 
-// function makeItClickable(){
-//     const everyColour = 
-//     const headingColour = document.querySelector(`h1`)
-//     headingColour.addEventListener(`click`, everyColour);
-
-// }
+function makeItClickable() {
+    const headingColour = document.querySelector(`h1`);
+    function everyColour () {
+        const rgb = [];
+        for (i = 0; i < 3; i++) {
+            const randRgb = Math.floor(Math.random() * 256);
+            rgb.push(randRgb);
+        }
+        // console.log(rgb)
+        return headingColour.style.color = `rgb(${rgb[0]}, ${rgb[1]}, ${rgb[2]})`;
+    }
+    // console.log(everyColour())
+    return headingColour.addEventListener(`click`, everyColour);
+}
 
 // /* EXERCISE 10
 // Change the footer text with something else when the user clicks on it.
 // */
 
-// function changeFooterText(){
-
-// }
+function changeFooterText(){
+    const footerText = document.querySelector(`footer p`);
+    function newFooterText() {
+    footerText.innerText = `Something else`;
+    return footerText;
+    }
+    return footerText.addEventListener(`click`, newFooterText);
+}
 
 // /* EXERCISE 11
 // Attach an event listener to the input field in the page for console logging its value just after any keystroke.
 // */
 
-// const inputField = document.getElementById('input-field')
-// ...
+const inputField = document.getElementById('input-field');
+function logInput() {
+    const textFieldValue = inputField.value;
+    console.log(textFieldValue);    
+}
+inputField.addEventListener(`keyup`, logInput);
 
 // /* EXERCISE 12
 // Create a new welcome alert message when the page successfully loads.
 // */
 
-// window.onload = function(){
-
-// }
+window.onload = function(){
+    alert(`Page has succesfully loaded`);
+}
 
 // /* EXERCISE 13
 // Use HTML5 tags to divide the content of the page in a more semantic way.
